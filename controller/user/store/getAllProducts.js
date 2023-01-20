d0d50c82a5b9e4f457ef81c26e6b523d32fc991f
unknown <dani3morcos@gmail.com>
2023-01-20 14:14:21 +0200

    leptop changes

--- controller/user/store/getAllProducts.js
+++ controller/user/store/getAllProducts.js
@@ -1,11 +1,14 @@
 const mysql = require("../../../models/admin/menu/menu");
+const servingWays = require("../../../models/admin/servingway");
 
 
 
 const selectStoreProduct = async (req, res, next) => {
     try {
     let allItems = await mysql.selectAllActive();
-    res.json({allItems:allItems[0]});
+   let servingWaysList = await servingWays.selectAll()
+   
+    res.json({allItems:allItems[0],servingWaysList:servingWaysList[0]});
     } catch (e) {
         res.json({
             msg:{msg:e.message,type:'bad'}
