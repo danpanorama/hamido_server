e95b60a071b9533273728e052af4a1d6683bf2f4
danpanorama <62213659+danpanorama@users.noreply.github.com>
2023-01-24 16:30:02 +0200

    server

--- controller/user/store/getAllProducts.js
+++ controller/user/store/getAllProducts.js
@@ -1,14 +1,16 @@
 const mysql = require("../../../models/admin/menu/menu");
 const servingWays = require("../../../models/admin/servingway");
+const salads = require("../../../models/admin/salads");
 
 
 
 const selectStoreProduct = async (req, res, next) => {
     try {
-    let allItems = await mysql.selectAllActive();
+    let allItems = await mysql.selectAll();
    let servingWaysList = await servingWays.selectAll()
+   let saladslist = await salads.selectAll()
    
-    res.json({allItems:allItems[0],servingWaysList:servingWaysList[0]});
+    res.json({allItems:allItems[0],servingWaysList:servingWaysList[0],saladslist:saladslist[0]});
     } catch (e) {
         res.json({
             msg:{msg:e.message,type:'bad'}
